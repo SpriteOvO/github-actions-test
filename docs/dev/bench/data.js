f5217234a9ea1ebe32dba06ac83b0a3a7a457cab
github-action-benchmark <github@users.noreply.github.com>
2024-06-12 21:36:12 +0000

    add spdlog-rs on Linux (cargo) benchmark result for c84c2d6e9ffc34cf9714e121a81396a8572b7e95

---
+++ docs/dev/bench/data.js
@@ -0,0 +1,62 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1718228172948,
+  "repoUrl": "https://github.com/SpriteOvO/github-actions-test",
+  "entries": {
+    "spdlog-rs on Linux": [
+      {
+        "commit": {
+          "author": {
+            "email": "SpriteOvO@gmail.com",
+            "name": "Asuna",
+            "username": "SpriteOvO"
+          },
+          "committer": {
+            "email": "SpriteOvO@gmail.com",
+            "name": "Asuna",
+            "username": "SpriteOvO"
+          },
+          "distinct": true,
+          "id": "c84c2d6e9ffc34cf9714e121a81396a8572b7e95",
+          "message": "Update ci.yml",
+          "timestamp": "2024-06-13T05:35:03+08:00",
+          "tree_id": "e6399b9f0a4b3533ae14111b4f4bf5ebfa1b5e4b",
+          "url": "https://github.com/SpriteOvO/github-actions-test/commit/c84c2d6e9ffc34cf9714e121a81396a8572b7e95"
+        },
+        "date": 1718228172629,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_1_file",
+            "value": 214.33,
+            "range": "± 1.54",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_2_file_async",
+            "value": 197.21,
+            "range": "± 1.77",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_3_rotating_file_size",
+            "value": 220.13,
+            "range": "± 47.45",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_4_rotating_daily",
+            "value": 219.73,
+            "range": "± 15.20",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_5_level_off",
+            "value": 2.78,
+            "range": "± 0.02",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
